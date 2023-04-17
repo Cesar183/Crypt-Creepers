11cdfb4a2ec7138aab78a8d575b440c40d91e38d
Cesar <cesar_a_mar_@hotmail.com>
2023-04-17 17:19:13 -0500

    Agregando Enemigo

--- Scripts/Bullet.cs
+++ Scripts/Bullet.cs
@@ -18,4 +18,12 @@ public class Bullet : MonoBehaviour
             Destroy(gameObject);
         }
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Enemy")
+        {
+            collision.GetComponent<Enemy>().TakeDamage();
+            Destroy(gameObject);
+        }
+    }
 }
---
+++ Scripts/Enemy.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] int health = 1;
+    public void TakeDamage()
+    {
+        health--;
+    }
+}
