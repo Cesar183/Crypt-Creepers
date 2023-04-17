3bf1db99b40d1844d058925bb562678c24a864b7
Cesar <cesar_a_mar_@hotmail.com>
2023-04-17 17:38:29 -0500

    Actualizar al enemigo para que persiga al player

--- Scripts/Enemy.cs
+++ Scripts/Enemy.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    Transform player;
     [SerializeField] int health = 1;
+    [SerializeField] float speed = 1;
+    private void Start()
+    {
+        player = FindObjectOfType<Player>().transform;
+    }
+    private void Update()
+    {
+        Vector2 direction = player.position - transform.position;
+        transform.position += (Vector3)direction * Time.deltaTime * 1;
+    }
     public void TakeDamage()
     {
         health--;
