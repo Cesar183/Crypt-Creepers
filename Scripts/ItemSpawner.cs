ba2ee79fe47cd531fda6a806ff43e555649bd2a0
Cesar <cesar_a_mar_@hotmail.com>
2023-04-18 18:34:22 -0500

    Agrendo powerUp, para incrementar potencia al player

--- Scripts/ItemSpawner.cs
+++ Scripts/ItemSpawner.cs
@@ -7,9 +7,12 @@ public class ItemSpawner : MonoBehaviour
     [SerializeField] GameObject checkPointPrefab;
     [SerializeField] int checkpointSpawnDelay = 10;
     [SerializeField] float spawnRadius = 7;
+    [SerializeField] GameObject[] powerUpPrefab;
+    [SerializeField] int powerUpSpawnDelay = 12;
     void Start()
     {
         StartCoroutine(SpawnCheckpointRoutine());
+        StartCoroutine(SpawnPowerUpRoutine());
     }
     IEnumerator SpawnCheckpointRoutine()
     {
@@ -20,5 +23,14 @@ public class ItemSpawner : MonoBehaviour
             Instantiate(checkPointPrefab, randomPosition, Quaternion.identity);
         }
         
+    }
+    IEnumerator SpawnPowerUpRoutine()
+    {
+        while(true){
+            yield return new WaitForSeconds(powerUpSpawnDelay);
+            Vector2 randomPosition = Random.insideUnitCircle * spawnRadius;
+            int random = Random.Range(0, powerUpPrefab.Length);
+            Instantiate(powerUpPrefab[random], randomPosition, Quaternion.identity);
+        }
     }
 }
